bb0353c95b83240d761e5fd04c1b59e7733c1da7
Royer G <royjuni3431@gmail.com>
2021-02-08 07:01:50 -0500

    feat: add smoot effect to open and close the burger button

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const burgerButton = document.getElementsByClassName('burger-button')[0]
-const menu = document.querySelector('header > .menu') 
-let menuCollapsed = false
-const menuLinks = document.getElementsByClassName('link')
+const burgerButton = document.getElementsByClassName("burger-button")[0];
+const menu = document.querySelector("header > .menu");
+let menuCollapsed = false;
+const menuLinks = document.getElementsByClassName("link");
 
 const openMenu = () => {
-    menuCollapsed = true
-    burgerButton.src = 'assets/img/close.svg'
-    burgerButton.style.width = '25px'
-    menu.style.display = 'flex'
-}
+  menuCollapsed = true;
+  burgerButton.src = "assets/img/close.svg";
+  burgerButton.style.width = "25px";
+  menu.style.visibility = "visible";
+  menu.style.opacity = "1";
+};
 
 const closeMenu = () => {
-    menuCollapsed = false
-    burgerButton.style.width = '35px'
-    burgerButton.src = 'assets/img/burger.svg'
-    menu.style.display = 'none'
-}
+  menuCollapsed = false;
+  burgerButton.style.width = "35px";
+  burgerButton.src = "assets/img/burger.svg";
+  menu.style.visibility = "hidden";
+  menu.style.opacity = "0";
+};
 
-burgerButton.addEventListener('click', () => {
-    if (menuCollapsed === false) {
-        openMenu()
-    } else {
-        closeMenu()
-    }
-})
+burgerButton.addEventListener("click", () => {
+  if (menuCollapsed === false) {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+});
 
 for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener('click', () => {
-        if (menuCollapsed === true) {
-            closeMenu()
-        }
-    })
+  menuLinks[i].addEventListener("click", () => {
+    if (menuCollapsed === true) {
+      closeMenu();
+    }
+  });
 }
